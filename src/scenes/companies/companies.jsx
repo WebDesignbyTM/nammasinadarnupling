3297776d068ec947d31bf6aa379dec8adb3bba8d
Cipri <ciprimarin2001@gmail.com>
2020-04-23 19:29:42 +0300

    Made the company tables. Companies should decide details

--- src/scenes/companies/companies.jsx
+++ src/scenes/companies/companies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import Header from '../../components/header/header.jsx';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -9,11 +9,7 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { getCompanies } from '../../api/requests.js';
 import useForm from '../../utils/useForm.js';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import MaterialTable from 'material-table';
 import CloseIcon from '@material-ui/icons/Close';
 import Slide from '@material-ui/core/Slide';
 import Dialog from '@material-ui/core/Dialog';
@@ -25,21 +21,23 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import CompanyProfile from '../../components/companyprofile/companyprofile.jsx';
-
+import InputAdornment from '@material-ui/core/InputAdornment';
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
   root:{
     display:'flex',
     backgroundColor:theme.palette.primary.main,
     flex:1,
+    justifyContent:'center'
   },
   toolbarReplace: {
     ...theme.mixins.toolbar
   },
   content: {
-    flex:1,
     flexDirection:'column',
     padding: theme.spacing(3),
+    flexBasis:500
   },
   paper: {
     padding: theme.spacing(2),
@@ -68,15 +66,19 @@ export default function Companies(props) {
   const [open, setOpen] = useState(false);
   const [selectedCompany, setSelectedCompany] = useState({});
   const [found, setFound] = useState([]);
-  const {handleSubmit, handleInputChange, input} = useForm(() => {
-    let payload = { name: input.company_name };
-    console.log(payload);
-    getCompanies(payload).then(res => {
+
+  const searchCompanies = (key) => {
+    getCompanies({name:key}).then(res=>{
       setFound(res);
     }, rej => {
       console.log(rej);
-    });
-  });
+    })
+  }
+
+  useEffect(()=>{
+    searchCompanies("");
+  },[]);
+
 
   const checkProfile = (comp) => {
     setSelectedCompany(found[comp]);
@@ -90,70 +92,28 @@ export default function Companies(props) {
   return (
     <div className={classes.root}>
       <Header title='Companies'/>
-      <main className={classes.content}>
+      <div className={classes.content}>
         <div className={classes.toolbarReplace} />
 
-        <form className={classes.searchForm} onSubmit={handleSubmit}>
-          <div>
-            <TextField label="Search company" name="company_name" onChange={handleInputChange}/>
-          </div>
-            <Button type="submit" variant="contained" color="secondary">
-              Submit
-            </Button>
-          </form>
+        <Paper>
+          <MaterialTable
+            columns={[
+              { title: 'Name',    field: 'company_name' },
+              { title: 'Phone',   field: 'company_phone' },
+              { title: "E-mail",  field: 'company_email'},
+              { title: "Address", field: 'company_address'},
+              { title: "CUI",     field: 'company_cui'}
+            ]}
+            detailPanel={rowData=>{
+              console.log(rowData)
+              return(<CompanyProfile selectedCompany={rowData}/>)
+            }}
+            data={found}
+            title="Companies"
+          />
+        </Paper>
 
-          
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Company id</TableCell>
-              <TableCell align="right">Company name</TableCell>
-              <TableCell align="right">Company cui</TableCell>
-              <TableCell align="right">Company profile</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {found.map((row, idx) => (
-              <TableRow key={row.id}>
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell align="right">{row.company_name}</TableCell>
-                <TableCell align="right">{row.company_cui}</TableCell>
-                <TableCell align="right">
-                  <Button variant="contained" color="secondary" onClick={() => checkProfile(idx)}>
-                    Check profile
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-
-        <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
-          <AppBar className={classes.appBar}>
-            <Toolbar>
-              <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
-                <CloseIcon />
-              </IconButton>
-              <Typography variant="h6" className={classes.title}>
-                { selectedCompany.company_name }
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <CompanyProfile props={selectedCompany} />
-          {/* <List>
-            <ListItem button>
-              <ListItemText primary="Phone ringtone" secondary="Titania" />
-            </ListItem>
-            <Divider />
-            <ListItem button>
-              <ListItemText primary="Default notification ringtone" secondary="Tethys" />
-            </ListItem>
-          </List> */}
-        </Dialog>
-
-      </main>
+      </div>
 
     </div>
   )
